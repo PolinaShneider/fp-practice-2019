4f6c48088b9731061a4d6b4f75d4e18a43a8c266
Polina Shneider <beatrixlinkoln@gmail.com>
2020-01-14 00:16:34 +0300

    Task 5 solution

--- Task5_1.hs
+++ Task5_1.hs
@@ -3,13 +3,7 @@ module Task5_1 where
 import Todo(todo)
 
 -- Структура двусвязного списка из лекции про ленивость
-
-data DList a = DNil 
-             | DCons { 
-                left :: (DList a), 
-                current :: a, 
-                right :: (DList a) 
-             }
+data DList a = DNil | DCons { left :: DList a, current :: a, right :: DList a }
 
 instance (Show a) => Show (DList a) where
     show it = "[" ++ showBody it ++ "]"
@@ -23,21 +17,58 @@ instance (Eq a) => Eq (DList a) where
     _ == _ = False
 
 list2dlist :: [a] -> DList a
-list2dlist lst = list2dlist' DNil lst
+list2dlist = list2dlist' DNil
 
 list2dlist' :: DList a -> [a] -> DList a
 list2dlist' _ [] = DNil
-list2dlist' left (h: t) = 
-    let rec = DCons left h (list2dlist' rec t)
-    in rec
-
+list2dlist' left (h:t) =
+  let rec = DCons left h (list2dlist' rec t)
+   in rec
 
 -- Реализуйте функции индексирования, вставки и удаления элементов
 index :: DList a -> Int -> a
-index = todo
+index DNil _ = error "Index out of bounds"
+index (DCons _ x r) i
+  | i == 0 = x
+  | otherwise = index r (i - 1)
 
 insertAt :: DList a -> Int -> a -> DList a
-insertAt list index value = todo
+-- Список пустой: вставляем в нулевой элемент
+insertAt DNil _ value = DCons DNil value DNil
+insertAt (DCons left val right) i value
+-- Нашли нужный индекс. В следующий надо осуществить вставку.
+-- 1) Создаем элемент
+-- 2) Переносим текущее значение в правую часть
+  | i == 1 =
+    let rec = DCons left val (insertAt' rec value right)
+     in rec
+-- Вставка в 0 элемент
+  | i == 0 =
+    let rec = DCons DNil value (insertAt' rec val right)
+     in rec
+-- Если вставляем в индекс, который больше длины списка, то ошибка, иначе продолжаем
+  | i /= 1 =
+    case right of
+      DNil -> error "List index is out of bounds"
+      _    -> DCons left val (insertAt right (i - 1) value)
+
+insertAt' :: DList a -> a -> DList a -> DList a
+-- Обновляем последний элемент
+insertAt' left val DNil = DCons left val DNil
+-- Обновляем все правые элементы, прокидывая "себя" в правый элемент
+insertAt' left val (DCons left' val' right) =
+  let rec = DCons left val (insertAt' rec val' right)
+   in rec
 
 removeAt :: DList a -> Int -> DList a
-removeAt list index = todo
+-- Слева пусто
+removeAt DNil _ = error "List is empty. Nothing to remove"
+-- Справа пусто
+removeAt (DCons _ _ DNil) i
+  | i == 0 = DNil
+  | otherwise = error "Incorrect index value"
+
+-- Удаление нулевого индекса
+removeAt (DCons left current (DCons _ val right)) 0 = DCons left val right
+-- Общий случай
+removeAt (DCons left current right) index = DCons left current (removeAt right (index - 1))
--- Task5_2.hs
+++ Task5_2.hs
@@ -8,8 +8,17 @@ data Zipper a = Zipper [a] [a]
 
 -- Реализуйте экземпляры классов Show и Eq для этого типа
 
+instance (Show a) => Show (Zipper a) where
+    show (Zipper l r) = show (reverse l) ++ show r  
+
+instance (Eq a) => Eq (Zipper a) where
+    (==) z1 z2 = toList z1 == toList z2 
+
+toList :: Zipper a -> [a]
+toList (Zipper l r) = l ++ r
+
 fromList :: [a] -> Zipper a
-fromList lst = Zipper [] lst
+fromList = Zipper []
 
 goRight :: Zipper a -> Zipper a
 goRight z@(Zipper _ []) = z
@@ -34,11 +43,24 @@ removeLeft (Zipper (_:lt) r) = Zipper lt r
 -- Используя приведённые выше функции, реализуйте функцию конкатенации
 -- вставки подсписка в середину и выделения подсписка
 
-concat :: Zipper a -> Zipper a -> Zipper a
-concat left right = todo
+concat' :: Zipper a -> Zipper a -> Zipper a
+concat' left right = Zipper (toList left) (toList right)
+
+insertManyAt' :: Int -> Zipper a -> Zipper a -> Zipper a
+insertManyAt' 0 (Zipper left right) (Zipper left_ right_) = Zipper (left_ ++ left) (right ++ right_)
+insertManyAt' index target source = insertManyAt' (index - 1) target (goRight source)
 
 insertManyAt :: Int -> Zipper a -> Zipper a -> Zipper a
-insertManyAt index what into = todo
+insertManyAt index (Zipper [] []) into = into
+insertManyAt index target (Zipper [] []) = target
+insertManyAt index target (Zipper left right) = if index < 0 || index > length(left ++ right)
+                                         then error "Incorrect Index"
+                                         else insertManyAt' index target (Zipper [] (left ++ right))
 
 subZipper :: Int -> Int -> Zipper a -> Zipper a
-subZipper from to input = todo
+subZipper from to (Zipper [] []) = error "Zipper is empty"
+subZipper from to (Zipper left right)
+                        | from < 0 || from > length(left ++ right) = error "From is greater than list length"
+                        | to < 0 || to > length(left ++ right) = error "To is greater than list length"
+                        | from > to = error "Error from > to"
+                        | otherwise = Zipper [] $take (to - from + 1) $drop from $concat [left,right]
