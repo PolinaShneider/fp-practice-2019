68610292d76175ad9d952f40cf0c6947daba01fc
Polina Shneider <beatrixlinkoln@gmail.com>
2020-01-14 23:06:25 +0300

    Task 4 solution

--- Task4_1.hs
+++ Task4_1.hs
@@ -11,3 +11,23 @@ module Task4_1 where
 data FunMonad a = FunMonad { fun :: String -> a }
 
 -- реализуйте классы `Functor`, `Applicative` и `Monad` для типа `FunMonad`
+
+-- Functor занимается преобразованием элементов одного множества в элементы другого множества
+-- Здесь fmap применяет функцию func ко всем элементам с типом `el`, хранящимся в контейнере.
+instance Functor FunMonad where
+    fmap func (FunMonad x) = FunMonad (\el -> func (x el))
+
+-- Applicative оборачивает в контекст и аргумент, и функцию, которая к нему применяется 
+instance Applicative FunMonad where
+-- pure помещает значения в контекст
+-- То есть независимо от аргумента мы получим FunMonad
+    pure x = FunMonad (\a -> x)
+-- <*> применяет функцию, помещенную в контекст func1 к значению, помещенному в контекст func2
+-- То есть мы сначала достаем оба параметра из контекста, а затем применяем полученную функцию к значению
+    (FunMonad func1) <*> (FunMonad func2) = FunMonad (\a -> func1 a (func2 a))
+
+instance Monad FunMonad where
+-- Результат вызова `return` - монадический контейнер
+    return x = FunMonad (\a -> x)
+-- >>= применяет функцию func к FunMonad и возвращает результат, обернутый в FunMonad
+    FunMonad x >>= func = FunMonad (\a -> fun (func (x a)) a)
--- Task4_2.hs
+++ Task4_2.hs
@@ -11,3 +11,27 @@ data FourOf a = FourOf a a a a deriving(Show,Eq)
 -- реализуйте классы `Functor`, `Applicative` и `Monad` для типа `FourOf`
 -- таким образом, что
 -- do { x <- FourOf 1 2 3 4; y <- FourOf 4 6 7 8; return $ x + y } === FourOf 5 8 10 12
+
+-- Functor, отображает элементы одного множества на элементы другого.
+-- Применим функцию func к каждом элементу FourOf
+instance Functor FourOf where
+    fmap func (FourOf a b c d) = FourOf (func $ a) (func $ b) (func $ c) (func $ d)
+
+instance Applicative FourOf where
+-- Нам не важен контекст — всегда будем возвращать значение типа FourOf
+    pure el = FourOf el el el el
+-- Применим функции (func1 func2 func3 func4) в контексте к значениям в контексте (a1 a2 a3 a4)
+-- То есть мы применяем каждый элемент второй четверки (a, b, c, d) к функциям из первой четверки
+    (<*>) (FourOf func1 func2 func3 func4) (FourOf a b c d) = FourOf (func1 $ a) (func2 $ b) (func3 $ c) (func4 $ d)
+
+instance Monad FourOf where
+-- Обернем значение в монаду
+    return x = FourOf x x x x
+-- Для последовательного применения функции в определении `>>=` используется вспомогательный pattern matching.
+-- С его помощью аргументы конструктора по порядку упаковываются в монаду. 
+-- После поэлементной распаковки получаем конструктор `FourOf` со значениями, в которых учтен результат отображения func.     
+    FourOf a b c d >>= func = FourOf a' b' c' d' where
+        FourOf a' _ _ _ = func a 
+        FourOf _ b' _ _ = func b
+        FourOf _ _ c' _ = func c
+        FourOf _ _ _ d' = func d
